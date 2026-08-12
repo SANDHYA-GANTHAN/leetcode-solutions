94855c83a76341058214c5336b5235a707c92411
SANDHYA GANTHAN <sandhya.ganthan@gmail.com>
2026-08-12 19:05:27 +0530

    Time: 3 ms (6.49%) | Memory: 19.3 MB (15.82%) - LeetSync

---
+++ 812-rotate-string/rotate-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s)!=len(goal):
+            return False
+        a=s+s
+        if goal in a:
+            return True
+        else:
+            return False
