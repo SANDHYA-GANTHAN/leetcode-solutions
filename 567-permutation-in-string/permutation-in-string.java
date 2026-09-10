5e7d2a39e597b3a75517b705ac9d4e84ffe9f19f
SANDHYA GANTHAN <sandhya.ganthan@gmail.com>
2026-09-10 13:58:20 +0530

    Time: 6 ms (87.45%) | Memory: 43.7 MB (92.94%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] count1=new int[26];
+        int[] count2=new int[26];
+        if(s1.length()>s2.length()){
+            return false;
+        }
+
+        for(int i=0;i<s1.length();i++)
+            count1[s1.charAt(i)-'a']++;
+        int k=s1.length();
+
+        for(int i=0;i<k;i++)
+            count2[s2.charAt(i)-'a']++;
+
+        if(Arrays.equals(count1,count2))
+            return true;
+
+        for(int i=s1.length();i<s2.length();i++){
+            count2[s2.charAt(i-k)-'a']--;
+            count2[s2.charAt(i)-'a']++;
+            if(Arrays.equals(count1,count2))
+                return true;
+        }
+        return false;
+        
+
+    }
+}
