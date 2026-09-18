8b0db0cd6e6ef1d2b5d3f78e80591ab3b06afdc3
SANDHYA GANTHAN <sandhya.ganthan@gmail.com>
2026-09-18 10:42:59 +0530

    Time: 14 ms (51.66%) | Memory: 59.8 MB (26.31%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int goal) {
+        return atMost(nums,goal)-atMost(nums,goal-1);
+    }
+    private int atMost(int[] nums,int goal){
+        if(goal<0)return 0;
+        int l=0,c=0;
+        int count=0;
+        for(int r=0;r<nums.length;r++){
+            if(nums[r]%2!=0)
+            count++;
+            while(count>goal){
+                if(nums[l]%2!=0)
+                    count--;
+                l++;
+            }
+            c+=r-l+1;
+        }
+        return c;
+    }
+}
